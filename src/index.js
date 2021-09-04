dc093e4b2ba2a40b6685052ab9e91e01fd2aba03
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-09-04 02:03:05 -0700

    Add production and dev scripts to package.json

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const sauceRouter = require('./routes/sauceRoutes');
 //Middlware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//Console whether app is running in development/production mode
+console.log(process.env.NODE_ENV);
 
 //= =====================================================
 // DATABASE
