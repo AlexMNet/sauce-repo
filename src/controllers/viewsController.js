862916045321368549ab308f3ba01fa048b1d1b0
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-01-04 02:07:59 -0800

    populate users for overview view route. Add card buttons

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -2,7 +2,7 @@ const Sauce = require('../models/sauceModel');
 const asyncHandler = require('../utils/asyncHandler');
 
 exports.getOverview = asyncHandler(async (req, res, next) => {
-  const sauces = await Sauce.find({});
+  const sauces = await Sauce.find({}).populate('user');
 
   res.status(200).render('overview', {
     title: 'All Sauces',
