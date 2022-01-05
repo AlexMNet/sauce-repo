beedb9a7c0398f9db74c37eacc260a357866ff79
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-01-05 00:11:00 -0800

    Add dynamic text under H1 in overview page. Add capitalize function to pug template to capitalize username

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -1,14 +1,15 @@
 const Sauce = require('../models/sauceModel');
 const asyncHandler = require('../utils/asyncHandler');
+const { capitalize } = require('../utils/capitalize');
 
 exports.getOverview = asyncHandler(async (req, res, next) => {
   const sauces = await Sauce.find({
-    user: '61c4dfc0d05360d123a0366e',
+    user: '61ca8c13b0501da2b4072592',
   }).populate('user');
 
-  console.log(sauces);
   res.status(200).render('overview', {
     title: 'All Sauces',
     sauces,
+    capitalize,
   });
 });
