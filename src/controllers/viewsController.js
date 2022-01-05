8d18e37efb6c69b35e101cc29e3f95818bdc402f
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-01-04 23:58:47 -0800

    fix overview route to only show a users sauces.

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -2,8 +2,11 @@ const Sauce = require('../models/sauceModel');
 const asyncHandler = require('../utils/asyncHandler');
 
 exports.getOverview = asyncHandler(async (req, res, next) => {
-  const sauces = await Sauce.find({}).populate('user');
+  const sauces = await Sauce.find({
+    user: '61c4dfc0d05360d123a0366e',
+  }).populate('user');
 
+  console.log(sauces);
   res.status(200).render('overview', {
     title: 'All Sauces',
     sauces,
